ef3f318a4669fdc47c88bf0c02af3015cf1aaa1f
Ilya <ilmobdev@gmail.com>
2023-12-08 21:30:20 +0100

    #1. Add User entity class

---
+++ src/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Entity;
+
+class User {
+    private string $id;
+    private string $login;
+    private string $password;
+
+    public function __construct(string $login, string $password)
+    {
+        $this->login = $login;
+        $this->password = $password;
+    }
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getLogin() {
+        return $this->login;
+    }
+    
+    public function getPassword() {
+        return $this->password;
+    }
+    
+    public function setId(string $id) {
+        $this->id = $id;
+    }
+    
+    public function setLogin(string $login) {
+        $this->login = $login;
+    }
+
+    public function setPassword(string $password) {
+        $this->password = $password;
+    }
+}
