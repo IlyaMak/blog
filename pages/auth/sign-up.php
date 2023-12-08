870fcc42bd210b9689fd3919e99473a8e9e681bc
Ilya <ilmobdev@gmail.com>
2023-12-08 21:31:40 +0100

    #1. Add a sign-up page

---
+++ pages/auth/sign-up.php
@@ -0,0 +1,32 @@
+<?php
+
+include '../../src/index.php';
+
+use Controller\AuthController;
+
+$isFailedRegistration = AuthController::signUp();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sign Up</title>
+</head>
+
+<body>
+	<h1>Sign Up</h1>
+	<form action="./sign-up.php" method="post">
+		<input type="text" name="login" placeholder="Login">
+		<input type="password" name="password" placeholder="Password">
+		<input type="password" name="repeatPassword" placeholder="Repeat Password">
+		<button type="submit">Save</button>
+	</form>
+	<?php if ($isFailedRegistration) : ?>
+		<span>Please, correct fields</span>
+	<?php endif; ?>
+</body>
+
+</html>
