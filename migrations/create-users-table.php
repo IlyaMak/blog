77bc2fc541a8439180c852657ec2cfe77fdf316e
Ilya <ilmobdev@gmail.com>
2023-12-08 21:29:55 +0100

    #1. Add a script for creating a users table

---
+++ migrations/create-users-table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Service\DatabaseConnector;
+
+function createUsersTable()
+{
+    $db = DatabaseConnector::getDatabaseConnection();
+    $db->query(
+        'CREATE TABLE users (
+            id int NOT NULL AUTO_INCREMENT,
+            login varchar(255) NOT NULL,
+            password varchar(255) NOT NULL,
+            PRIMARY KEY (id)
+        );'
+    );
+    $db = null;
+}
+
+createUsersTable();
