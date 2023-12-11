9a45b59b91075448e71efbe445c26b757d4c4cc9
Ilya <ilmobdev@gmail.com>
2023-12-11 16:26:03 +0100

    #7. Modify the tags-list page with the table with tags

--- pages/tag/tags-list.php
+++ pages/tag/tags-list.php
@@ -1,3 +1,26 @@
+<?php
+
+include '../../src/autoload.php';
+
+use App\Repository\TagRepository;
+use App\Service\DatabaseConnector;
+
+$db = DatabaseConnector::getDatabaseConnection();
+$tagRepository = new TagRepository($db);
+$tags = $tagRepository->getTags();
+
+$getParentTagName = function (int $tagId) use ($tags, $tagRepository) {
+	try {
+		echo $tagRepository->getTag(
+			$tags[$tagId]['parent_tag_id']
+		)['name'];
+	} catch (TypeError $e) {
+		echo '-';
+	}
+};
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -10,6 +33,22 @@
 <body>
 	<h1>Tags List</h1>
 	<a href="../tag/create-tag.php">Create tag</a>
+	<table>
+		<tr>
+			<th>Name</th>
+			<th>Parent tag</th>
+		</tr>
+		<?php for ($i = 0; $i <= count($tags) - 1; $i++) {
+			if ($tags[$i]['is_visible']) { ?>
+				<tr>
+					<td><?php echo $tags[$i]['name'] ?></td>
+					<td>
+						<?php $getParentTagName($i) ?>
+					</td>
+				</tr>
+		<?php }
+		} ?>
+	</table>
 </body>
 
 </html>
