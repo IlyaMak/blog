f8199d69c95c44eb6764fb0b5b392dd05581d318
Ilya <ilmobdev@gmail.com>
2023-12-19 17:11:08 +0100

    #18. Add a getPostTag and deletePostTag methods to the PostsTagsRepository

--- src/App/Repository/PostsTagsRepository.php
+++ src/App/Repository/PostsTagsRepository.php
@@ -26,4 +26,23 @@ class PostsTagsRepository
         $pdoStatement = $this->db->prepare("$sql;");
         $pdoStatement->execute();
     }
+
+    public function getPostTag(int $postId): array|bool
+    {
+        $pdoStatement = $this->db->prepare(
+            'SELECT * FROM posts_tags WHERE post_id = :postId'
+        );
+        $pdoStatement->bindParam('postId', $postId);
+        $pdoStatement->execute();
+        return $pdoStatement->fetch(PDO::FETCH_ASSOC, PDO::PARAM_INT);
+    }
+
+    public function deletePostTag(int $postId): void
+    {
+        $pdoStatement = $this->db->prepare(
+            'DELETE FROM posts_tags WHERE post_id = :postId'
+        );
+        $pdoStatement->bindParam('postId', $postId, PDO::PARAM_INT);
+        $pdoStatement->execute();
+    }
 }
