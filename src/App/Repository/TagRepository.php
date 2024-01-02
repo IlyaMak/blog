744886c094c3ccb3f2d1c0c04c27a56c727ec549
Ilya <ilmobdev@gmail.com>
2024-01-02 17:21:52 +0100

    #30. Add getLimitedVisibleTagsWithParentTagName method

--- src/App/Repository/TagRepository.php
+++ src/App/Repository/TagRepository.php
@@ -67,6 +67,22 @@ class TagRepository
         return $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function getLimitedVisibleTagsWithParentTagName(
+        int $offset,
+        int $limit
+    ): array|bool {
+        $pdoStatement = $this->db->prepare(
+            "SELECT t1.id, t1.name, COALESCE(t2.name, '-') as parent_tag_name FROM tags t1
+            LEFT JOIN tags t2 on t1.parent_tag_id = t2.id
+            WHERE t1.is_visible = 1
+            LIMIT :limit OFFSET :offset"
+        );
+        $pdoStatement->bindParam('limit', $limit, PDO::PARAM_INT);
+        $pdoStatement->bindParam('offset', $offset, PDO::PARAM_INT);
+        $pdoStatement->execute();
+        return $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     public function deleteTag(int $id): void
     {
         $pdoStatement = $this->db->prepare(
